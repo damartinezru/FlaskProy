5e2a345d2c06e68843687ca787855dd22d1a37ae
Daisy <dbgroldan@gmail.com>
2019-12-10 11:01:08 -0500

    Project reorganization

---
+++ app/model.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+class User(object):
+
+    def __init__(self, id, name, email, nick, passwd):
+        self._id = id
+        self._name = name
+        self._email = email
+        self._nick = nick
+        self._pass = passwd
+
+    def getName(self):
+        return self._name
+
+    def getEmail(self):
+        return self._email
+
+    def getNick(self):
+        return self._nick
+
+    def getPass(self):
+        return self._pass
