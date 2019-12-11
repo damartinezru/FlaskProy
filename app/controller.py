d375a5d28681552f039ba341849358cd2f408bf7
Daisy <dbgroldan@gmail.com>
2019-12-11 16:32:44 -0500

    Expiration of Session

--- app/controller.py
+++ app/controller.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template, request, redirect, url_for, flash, make_response
+from flask import Flask, render_template, request, redirect, url_for, flash, make_response, session
 import json
 from model import User
 from utils.hash_verification import Hash
@@ -8,7 +8,7 @@ from utils.token_session import Token
 app = Flask(__name__)
 app.secret_key = 'mysecretkey'
 token = Token()
-
+#resp = make_response(render_template('account.html', user = user))
 # User definition #
 #dbgroldanNick: 12345
 #gabNick: prueba
@@ -19,10 +19,11 @@ with open('docs/user.json') as json_file:
 @app.route('/')
 def index():
     exist_token = request.cookies.get('token')
-    if exist_token is not None:
-        if token.verify_token(exist_token, data_users):
-            user = token.getUser()
-            return render_template('account.html', user = user)
+    if 'nickname' in session:
+        if exist_token is not None:
+            if token.verify_token(exist_token, data_users):
+                user = token.getUser()
+                return render_template('account.html', user = user)
     return render_template('index.html')
 
 @app.route('/login_user', methods = ['POST'])
@@ -30,6 +31,7 @@ def login_user():
     if request.method == 'POST':
         nick = request.form['nick']
         passwd = request.form['pass']
+        session['nickname'] = nick
         for user in data_users:
             if user["nick"] == nick and Hash.verify_two_hash(user["hashed_passwd"], passwd):
                 print(user["name"])
@@ -42,8 +44,10 @@ def login_user():
 
 @app.route('/log_out')
 def log_out():
+    session.pop('nickname', None)
     flash('Has gone out of your account!')
     return redirect(url_for('index'))
+    #return respo
 
 if __name__ == '__main__':
     app.run(port = 3000, debug = True)
